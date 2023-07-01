56624a0cb61835a8710d9b507820ee4fef192a09
Vince489 <vincentcornelius489@gmail.com>
2023-07-01 11:36:41 -0400

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 require("./config/db");
 
 const express = require("express");
-const bodyParser = express.json;
 const cors = require("cors");
 const routes = require("./routes");
 
@@ -10,7 +9,7 @@ const routes = require("./routes");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser());
+app.use(express.json())
 app.use("/api/v1", routes);
 
 module.exports = app;
